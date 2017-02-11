1d68f522306f0b17389eaa3608cd90f9fd96cbd2
Andy Wooten <andrewdwooten@icloud.com>
2017-02-11 16:49:01 -0700

    Fix feature/model tests which were failing due to new requirement of category for every job.

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe "User sees one company" do
   scenario "a user sees a company" do
+    cat = Category.create(title: 'Test')
     company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
+    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver", category_id: cat.id)
 
     visit company_path(company)
 
--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe 'User deletes a job' do
   scenario 'a user can delete a job' do
+    cat = Category.create(title: 'Test')
     company = Company.create(name: "Apple")
     job = company.jobs.create(title: 'Web Developer',
                               level_of_interest: 8,
                               city: 'Denver',
-                              description: 'Awesome Stuff')
+                              description: 'Awesome Stuff',
+                              category_id: cat.id)
 
     visit company_job_path(company, job)
     expect { click_link "Delete This Job" }.to change(Job, :count).by(-1)
