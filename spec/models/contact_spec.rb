ea43d7f1518caedee49c56a09895a1f383f6d261
Andy Wooten <andrewdwooten@icloud.com>
2017-02-11 18:00:17 -0700

    Add validations to all attributes of contact.  Add model testing for validations as well as relationship to company.

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a name' do
+        company = Company.create(name: 'Test')
+        contact = Contact.new( position: 'boss',
+                               email: '123@test.com',
+                               company_id: company.id)
+        expect(contact).to be_invalid
+      end
+
+      it 'is invalid without a position' do
+        company = Company.create(name: 'Test')
+        contact = Contact.new( name: 'boss',
+                               email: '123@test.com',
+                               company_id: company.id)
+        expect(contact).to be_invalid
+      end
+
+      it 'is invalid without an email' do
+        company = Company.create(name: 'Test')
+        contact = Contact.new( position: 'boss',
+                               name: 'boss',
+                               company_id: company.id)
+        expect(contact).to be_invalid
+      end
+
+      it 'is invalid without a company' do
+        contact = Contact.new( position: 'boss',
+                               email: '123@test.com',
+                               name: 'boss')
+        expect(contact).to be_invalid
+      end
+
+      it 'is valid with all attributes' do
+        company = Company.create(name: 'Test')
+        contact = Contact.new( position: 'boss',
+                               email: '123@test.com',
+                               company_id: company.id,
+                               name: 'boss')
+        expect(contact).to be_valid
+      end
+    end
+  end
+
+  describe 'relationships' do
+    it 'belongs to a company' do
+      company = Company.create(name: 'Test')
+      contact = Contact.create( position: 'boss',
+                             email: '123@test.com',
+                             company_id: company.id,
+                             name: 'boss')
+      expect(contact).to respond_to(:company)
+    end
+  end
 end
